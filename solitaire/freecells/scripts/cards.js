b2ddbbb87733247abdff692d426158854ff66e67
0-micon <0.miconst@gmail.com>
2018-12-07 19:35:42 +0300

    Update cards.js
    + function deck(seed); A set of optionally shuffled playing cards.

--- solitaire/freecells/scripts/cards.js
+++ solitaire/freecells/scripts/cards.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 const Cards = (function () {
     const SUITS = 'SDCH';
     const SUIT_PLAY_NAMES = ['♠', '♦', '♣', '♥'];
@@ -10,6 +12,8 @@ const Cards = (function () {
 
     const SUIT_NUM = SUITS.length;
     const RANK_NUM = RANKS.length;
+
+    // Standard 52-card deck
     const CARD_NUM = SUIT_NUM * RANK_NUM;
 
     // Card index is defined as: suit + rank * SUIT_NUM
@@ -68,6 +72,33 @@ const Cards = (function () {
         return RANK_FULL_NAMES[rank(index)];
     }
 
+    // A set of optionally shuffled playing cards.
+    function deck(seed) {
+        const cards = [];
+        if (seed == undefined) {
+            for (let i = 0; i < CARD_NUM; i++) {
+                cards[i] = i;
+            }
+        } else {
+            // use LCG algorithm to pick up cards from the deck
+            // http://en.wikipedia.org/wiki/Linear_congruential_generator
+            const m = 0x80000000;
+            const a = 1103515245;
+            const c = 12345;
+
+            for (let i = 0; i < CARD_NUM; i++) {
+                seed = (a * seed + c) % m;
+
+                let card = seed % CARD_NUM;
+                while (cards.indexOf(card) >= 0) {
+                    card = (card + 1) % CARD_NUM;
+                }
+                cards.push(card);
+            }
+        }
+        return cards;
+    }
+
     return {
         SUITS: SUITS,
         RANKS: RANKS,
@@ -79,6 +110,7 @@ const Cards = (function () {
         index: index,
         rank: rank,
         suit: suit,
+        deck: deck,
 
         nameOf: nameOf,
         playNameOf: playNameOf,
